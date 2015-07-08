82fb6ba16bb8d6fd7221de468cb985dac21869e2
App Academy Student <student@appacademy.io>
2015-07-08 14:54:58 -0700

    Visitor model and distinctified number of visits

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,7 +1,6 @@
 class ShortenedUrl < ActiveRecord::Base
-  validates :short_url, presence: true, uniqueness: true
-  validates :user_id, presence: true
-  validates :long_url, presence: true
+  validates :short_url, uniqueness: true
+  validates :user_id, :short_url, :long_url, presence: true
 
   belongs_to(
     :submitter,
@@ -9,6 +8,15 @@ class ShortenedUrl < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  has_many(
+    :visits,
+    class_name: 'Visit',
+    foreign_key: :short_url_id,
+    primary_key: :id
+  )
+
+  has_many :visitors, -> { distinct }, through: :visits, source: :user
 
   def self.random_code
     code = SecureRandom.urlsafe_base64
@@ -21,6 +29,25 @@ class ShortenedUrl < ActiveRecord::Base
 
   def self.create_for_user_and_long_url!(user, long_url)
     self.create!(user_id: user.id, long_url: long_url, short_url: self.random_code)
+  end
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    uniq_ids = []
+    visits.each do |visit|
+      if visit.created_at > 10.minutes.ago && !uniq_ids.include?(visit.user_id)
+        uniq_ids << visit.user_id
+      end
+    end
+
+    uniq_ids.count
   end
 
 end
